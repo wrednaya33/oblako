aded40000df849203fa167f799f56f2667cff542
wrednaya33 <novikova@yandexlyceum.ru>
2021-03-19 16:36:58 +0300

    1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import os
+
 from flask import Flask
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)
 
 
 @app.route("/")
@@ -11,4 +11,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
